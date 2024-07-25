155a503d8b3e935b181f3e3dfd70c66e26803320
Juan Pimentel <juandbpimentel@gmail.com>
2024-07-24 22:30:28 -0300

    Deu certooooooo

--- estudos/teste-udp-punchhole_basico/teste/client.py
+++ estudos/teste-udp-punchhole_basico/teste/client.py
@@ -1,7 +1,6 @@
 import socket
 import struct
 import sys
-import time
 import threading
 
 def addr_to_bytes(addr):
@@ -10,9 +9,10 @@ def addr_to_bytes(addr):
 def bytes_to_addr(addr):
     return (socket.inet_ntoa(addr[:4]), struct.unpack('H', addr[4:])[0])
 
-def sendMessage(soc, message, peer):
-    time.sleep(5.0)
-    soc.sendto(message.encode(), peer)
+def recieveMessage(soc: socket):
+    print("Aguardando resposta do peer...")
+    data, addr = soc.recvfrom(1024)
+    print(f'{addr} disse: {data.decode()}')
 
 def udp_client(server, message):
     try:
@@ -21,25 +21,22 @@ def udp_client(server, message):
         soc.sendto(b'', server)
         
         print("Aguardando resposta do servidor...")
-        data, _ = soc.recvfrom(6)
+        data, addr = soc.recvfrom(6)
         peer = bytes_to_addr(data)
-        print('peer:', *peer)
-        data, _ = soc.recvfrom(1)
-        packagePostThread =  threading.Thread(target=sendMessage, args=(soc, message, peer))
-        packagePostThread.start()
-        print("Aguardando resposta do peer...")
-        data, addr = soc.recvfrom(1024)
-        print('{}:{} says {}'.format(*addr, data.decode()))
-        
+        threading.Thread(target=recieveMessage, args=(soc,)).start()
+        print('conectado ao peer:', *peer)
+        print('Envie uma mensagem para o peer...')
+        soc.sendto(message.encode(), peer)
     except socket.timeout:
         print("Operação de socket atingiu o timeout.")
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
 
 def main():
+    print("Iniciando whatsapp")
     host = sys.argv[1]
     port = int(sys.argv[2])
-    message = sys.argv[3]
+    message:str = input("Digite a mensagem que deseja enviar para o seu parceiro: ")
     print("tentando conectar to {}:{}".format(host, port))
     server_addr = (host, port)
     udp_client(server_addr, message)
