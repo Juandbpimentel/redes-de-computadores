c154842cd6841edb53f36e9116d40ff8be8e5652
juandbpimentel <juandbpimentel@gmail.com>
2024-07-25 14:05:26 -0300

    Mandando correcoes no server e client

--- estudos/teste-udp-punchhole_basico/teste/server.py
+++ estudos/teste-udp-punchhole_basico/teste/server.py
@@ -1,11 +1,6 @@
 import socket
 import struct
 
-def addr_to_bytes(addr):
-    return socket.inet_aton(addr[0]) + struct.pack('H', addr[1])
-
-def bytes_to_addr(addr):
-    return (socket.inet_ntoa(addr[:4]), struct.unpack('H', addr[4:])[0])
 
 def udp_server(addr):
     soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,10 +9,10 @@ def udp_server(addr):
     _, client_a = soc.recvfrom(0)
     _, client_b = soc.recvfrom(0)
     print('conectado:', client_a, client_b)
-    soc.sendto(addr_to_bytes(client_b), client_a)
-    soc.sendto(addr_to_bytes(client_a), client_b)
-    
+    soc.sendto(str(client_b).encode(), client_a)
+    soc.sendto(str(client_a).encode(), client_b)
+
 
 print('rodou')
-addr = ('0.0.0.0', 10001)
-udp_server(addr)
+address = ('0.0.0.0', 10001)
+udp_server(address)
