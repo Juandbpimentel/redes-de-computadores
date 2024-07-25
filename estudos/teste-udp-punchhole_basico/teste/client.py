5cb86d44abf70f25c8b4003c00f3e844a0d8317d
Juan Pimentel <juandbpimentel@gmail.com>
2024-07-24 22:07:50 -0300

    correção no sending de mensagens

--- estudos/teste-udp-punchhole_basico/teste/client.py
+++ estudos/teste-udp-punchhole_basico/teste/client.py
@@ -2,12 +2,17 @@ import socket
 import struct
 import sys
 import time
+import threading
 
 def addr_to_bytes(addr):
     return socket.inet_aton(addr[0]) + struct.pack('H', addr[1])
 
 def bytes_to_addr(addr):
     return (socket.inet_ntoa(addr[:4]), struct.unpack('H', addr[4:])[0])
+
+def sendMessage(soc, message, peer):
+    time.sleep(5.0)
+    soc.sendto(message.encode(), peer)
 
 def udp_client(server, message):
     try:
@@ -20,23 +25,11 @@ def udp_client(server, message):
         peer = bytes_to_addr(data)
         print('peer:', *peer)
         data, _ = soc.recvfrom(1)
-        if data.decode() == '2':
-            print("Aguardando resposta do peer...")
-            data, addr = soc.recvfrom(2048)
-            print('{}:{} says {}'.format(*addr, data.decode()))
-            time.sleep(2)
-            print("Enviando mensagem para o peer...")
-            soc.sendto(message.encode(), peer)
-            return
-        
-        if data.decode() == '1':
-            print("Enviando mensagem para o peer...")
-            soc.sendto(message.encode(), peer)
-            
-            print("Aguardando resposta do peer...")
-            data, addr = soc.recvfrom(2048)
-            print('{}:{} says {}'.format(*addr, data.decode()))
-            
+        packagePostThread =  threading.Thread(target=sendMessage, args=(soc, message, peer))
+        packagePostThread.start()
+        print("Aguardando resposta do peer...")
+        data, addr = soc.recvfrom(1024)
+        print('{}:{} says {}'.format(*addr, data.decode()))
         
     except socket.timeout:
         print("Operação de socket atingiu o timeout.")
