f3c5eae5a7ec5415b5f6f95fb8f53aaf4aecd0ac
Juan Pimentel <juandbpimentel@gmail.com>
2024-08-16 13:03:07 -0300

    Começando client do projeto final

---
+++ praticas/projeto-final/game/scripts/Globals.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var recieverSocket:PacketPeerUDP = null
+var serverCommunicationSocket:StreamPeerTCP = null
+var socketParDoDuelo:int = 0
+var enderecoParDuelo:String = ''
+func settarSocketUDP(endereco:String,porta:int):
+	pass
