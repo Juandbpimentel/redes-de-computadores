2fd56b341073cce243d8680b6e2da6c3816aacf7
Juan Pimentel <juandbpimentel@gmail.com>
2024-08-26 13:52:47 -0300

    Atualizando projeto do game

---
+++ praticas/projeto-final/game/scenes/LoginScreen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Globals.recieverSocket = PacketPeerUDP.new()
+	Globals.recieverSocket.bind(0,"*",2048)
+	
+	print(Globals.recieverSocket.is_bound())
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# 9223372012460702513
+	# 9223372012460702513
+	# 9223372012460702513
+	pass
--- praticas/projeto-final/game/scripts/Globals.gd
+++ praticas/projeto-final/game/scripts/Globals.gd
@@ -4,5 +4,15 @@ var recieverSocket:PacketPeerUDP = null
 var serverCommunicationSocket:StreamPeerTCP = null
 var socketParDoDuelo:int = 0
 var enderecoParDuelo:String = ''
-func settarSocketUDP(endereco:String,porta:int):
+
+func fazerUpnp(porta:int):
+	var upnp = UPNP.new()
+	upnp.discover()
+	if upnp.add_port_mapping(porta) != 0:
+		print("houve um erro ao fazer a alocaç")
+
+func conectarComServidor(endereco:String,porta:int):
+	pass
+	
+func conectarComPar(endereco:String,porta:int):
 	pass
